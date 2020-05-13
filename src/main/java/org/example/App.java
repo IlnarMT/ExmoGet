4de073723d443376dcfd1108d79ed1ba97c807fe
mukhametzanovIT <mukhametzyanov@cg.ru>
2020-05-13 14:28:32 +0300

    Refactoring code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ import org.example.model.OrderBook;
 import org.example.service.JsonParsService;
 import org.example.service.MyHttpService;
 
+import java.time.Instant;
 import java.util.HashMap;
 
 public class App {
@@ -19,32 +20,36 @@ public class App {
         OrderBook orderBook;
         int k=0;
         String url;
+        Instant instant=Instant.now();
+        Long currentUnixTime=instant.getEpochSecond();
 
-        for (int i=0;i<1;i++){
+        for (int i=0;i<10000000;i++){
 
             url="https://api.exmo.me/v1.1/order_book";
             String strJsonTest=myHttpService.doPostRequest(url,new HashMap<String, String>() {{
                 put("pair", "BTC_USD");
                 put("limit", "50");
             }});
-            System.out.println(strJsonTest);
+            //System.out.println(strJsonTest);
             orderBook=jsonParsService.parsJsonToOrderBook(strJsonTest);
             connectDb.insertIntoOrderBooks(TradePairType.BTC_USD,orderBook);
 
-            if (i=)
-            url="https://api.exmo.me/v1.1/trades";
-            strJson=myHttpService.doPostRequest(url,new HashMap<String, String>() {{
-                put("pair", "BTC_USD");
-                /*put("pair", "BTC_EUR");*/
-            }});
+            if ((i % 30)==0) {
+                url = "https://api.exmo.me/v1.1/trades";
+                strJson = myHttpService.doPostRequest(url, new HashMap<String, String>() {{
+                    put("pair", "BTC_USD");
+                    /*put("pair", "BTC_EUR");*/
+                }});
+                carrencyTradePair=jsonParsService.parsJsonToCarrencyTradePair(strJson);
+                connectDb.insertIntoTableTransaction(TradePairType.BTC_USD,carrencyTradePair.BTC_USD);
+                k++;
+                System.out.println("Данные добавлены в таблицу carrencyTradePair "+k);
+            }
 
-            carrencyTradePair=jsonParsService.parsJsonToCarrencyTradePair(strJson);
-
-            connectDb.insertIntoTableTransaction(TradePairType.BTC_USD,carrencyTradePair.BTC_USD);
             //connectDb.insertIntoTableTransaction(TradePairType.BTC_EUR,carrencyTradePair.BTC_EUR);
-            Thread.sleep(30000);
-            k++;
-            System.out.println("Сделан цикл get "+k);
+            Thread.sleep(1000);
+
+            System.out.println("Данные добавлены в таблицу orderBook "+(i+1));
         }
     }
 
